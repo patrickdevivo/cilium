98b06321af8a0081a6465d58de3f80e2fa244447
Martynas Pumputis <m@lambda.lt>
2020-02-07 16:49:05 +0100

    test: Refactor deleteCiliumDS() usage
    - Move the function to helpers/kubectl.go.
    - Make sure that the function is called in AfterAll() of each suite.
    - Remove redundant deleteCiliumDS() calls.
    
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- test/k8sT/Services.go
+++ test/k8sT/Services.go
@@ -95,6 +95,7 @@ var _ = Describe("K8sServicesTest", func() {
 	})
 
 	AfterEach(func() {
+		kubectl.DeleteCiliumDS()
 		ExpectAllPodsTerminated(kubectl)
 	})
 
@@ -581,14 +582,12 @@ var _ = Describe("K8sServicesTest", func() {
 						ret = kubectl.CiliumExec(pod, "tc filter del dev "+nativeDev+" egress")
 						Expect(ret.WasSuccessful()).Should(BeTrue(), "Cannot remove egress bpf_netdev on %s", pod)
 					}
-					deleteCiliumDS(kubectl)
 					// Deploy Cilium as the next test expects it to be up and running
 					DeployCiliumAndDNS(kubectl, ciliumFilename)
 				})
 
 				Context("Tests with vxlan", func() {
 					BeforeAll(func() {
-						deleteCiliumDS(kubectl)
 						DeployCiliumOptionsAndDNS(kubectl, ciliumFilename, map[string]string{
 							"global.nodePort.enabled": "true",
 							"global.nodePort.device":  nativeDev,
@@ -610,7 +609,6 @@ var _ = Describe("K8sServicesTest", func() {
 
 				Context("Tests with direct routing", func() {
 					BeforeAll(func() {
-						deleteCiliumDS(kubectl)
 						DeployCiliumOptionsAndDNS(kubectl, ciliumFilename, map[string]string{
 							"global.nodePort.enabled":     "true",
 							"global.nodePort.device":      nativeDev,
@@ -658,7 +656,6 @@ var _ = Describe("K8sServicesTest", func() {
 				})
 
 				SkipItIf(helpers.DoesNotExistNodeWithoutCilium, "Tests with direct routing and DSR", func() {
-					deleteCiliumDS(kubectl)
 					DeployCiliumOptionsAndDNS(kubectl, ciliumFilename, map[string]string{
 						"global.nodePort.enabled":     "true",
 						"global.nodePort.device":      nativeDev,
